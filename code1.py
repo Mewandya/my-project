346f0d77cdb0ad720e9358bdcad2dfa4e0ff8b5a
Jayani Mewandya <64733914+Mewandya@users.noreply.github.com>
2021-10-08 23:22:54 +0530

    Updated code1.py
    changed the num variable to number

--- code1.py
+++ code1.py
@@ -1,6 +1,6 @@
 total=0
-num = input("Enter a number:")
-while num !=0:
-    total=total+num
-    num=input("Enter a number:")
+number = input("Enter a number:")
+while number !=0:
+    total=total+number
+    number=input("Enter a number:")
 print "Sum:",total
